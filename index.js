f133f4756b57e8d12ba653d6140d8de8df9e165b
Dean Howe <hideanhowe@gmail.com>
2025-12-19 09:50:55 +0000

    cleaner solution

--- index.js
+++ index.js
@@ -8,12 +8,18 @@
 
 // Uppercase and lowercase letters should be considered different characters.
 
+// Unique Characters
+// Given a string, determine if all the characters in the string are unique.
+
+// Uppercase and lowercase letters should be considered different characters.
+
 function allUnique(str) {
   console.log(str);
-  const arr = str.split('');
-  const charsSet = new Set();
+  // const arr = str.split("");
+  const charsSet = new Set(str);
 
-  arr.forEach((x) => charsSet.add(x));
+  // strings are iterable
+  //arr.forEach((x) => charsSet.add(x))
 
-  return arr.length === charsSet.size;
+  return str.length === charsSet.size;
 }
