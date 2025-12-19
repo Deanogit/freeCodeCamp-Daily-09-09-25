5f0ae847b546a81af63c487ac64466f35caeef12
Dean Howe <hideanhowe@gmail.com>
2025-12-19 09:49:05 +0000

    solution

--- index.js
+++ index.js
@@ -2,3 +2,18 @@
 // Given a string, determine if all the characters in the string are unique.
 
 // Uppercase and lowercase letters should be considered different characters.
+
+// Unique Characters
+// Given a string, determine if all the characters in the string are unique.
+
+// Uppercase and lowercase letters should be considered different characters.
+
+function allUnique(str) {
+  console.log(str);
+  const arr = str.split('');
+  const charsSet = new Set();
+
+  arr.forEach((x) => charsSet.add(x));
+
+  return arr.length === charsSet.size;
+}
